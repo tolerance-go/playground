7c7ab84e2297fa950803111a4f32b079d4fdcc6e
tolerance-go <892728595@qq.com>
2022-08-25 22:03:47 +0800

    chore: temp

--- src/pages/Workbench/SiderLeft/ComMaterial/TreeItem.tsx
+++ src/pages/Workbench/SiderLeft/ComMaterial/TreeItem.tsx
@@ -1,6 +1,6 @@
 import { RelationTreeItem } from '@/utils/treeUtils/makeTreeWithRelation';
 import { useModel } from '@umijs/max';
-import { Dropdown, Menu, Typography } from 'antd';
+import { Dropdown, Menu, Row, Tag, Tooltip, Typography } from 'antd';
 
 export const TreeItem = ({
   record,
@@ -23,33 +23,47 @@ export const TreeItem = ({
   );
 
   return (
-    <Dropdown
-      trigger={['contextMenu']}
-      overlay={
-        <Menu
-          items={[
-            {
-              key: 'extend',
-              label: '派生',
-              onClick: () => {},
-            },
-            {
-              key: 'remove',
-              label: '删除',
-              danger: true,
-              onClick: () => {
-                requestRemove(record.id);
+    <Row justify="space-between" align="middle">
+      <Dropdown
+        trigger={['contextMenu']}
+        overlay={
+          <Menu
+            items={[
+              {
+                key: 'extend',
+                label: '派生',
+                onClick: () => {},
               },
-            },
-          ]}
-        />
-      }
-    >
-      <div>
-        <Typography.Text mark={highlightId === record.id}>
-          {record.name}
-        </Typography.Text>
-      </div>
-    </Dropdown>
+              {
+                key: 'remove',
+                label: '删除',
+                danger: true,
+                onClick: () => {
+                  requestRemove(record.id);
+                },
+              },
+            ]}
+          />
+        }
+      >
+        <Tag>
+          <Typography.Text mark={highlightId === record.id}>
+            {record.name}
+          </Typography.Text>
+          {record.desc && (
+            <Typography.Paragraph type="secondary" style={{ marginBottom: 0 }}>
+              {record.desc}
+            </Typography.Paragraph>
+          )}
+        </Tag>
+      </Dropdown>
+      {record.usedInPageIds?.length ? (
+        <Tooltip title={`当前组件被 ${record.usedInPageIds.length} 个页面使用`}>
+          <Typography.Text type="secondary">
+            {record.usedInPageIds.length}
+          </Typography.Text>
+        </Tooltip>
+      ) : null}
+    </Row>
   );
 };
